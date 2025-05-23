685b7c7277000c4db68a791eb4a00573ec768ec6
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-05-23 22:52:43 +0530

    Backend - getPlaylistDetails controller added

--- backend/src/controller/playlist.controller.js
+++ backend/src/controller/playlist.controller.js
@@ -45,7 +45,35 @@ export const getPlayAllListDetails = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch playlist" });
   }
 };
-export const getPlayListDetails = async (req, res) => {};
+export const getPlayListDetails = async (req, res) => {
+  const { playlistId } = req.params;
+
+  try {
+    const playList = await db.playlist.findUnique({
+      where: { id: playlistId, userId: req.user.id },
+      include: {
+        problems: {
+          include: {
+            problem: true,
+          },
+        },
+      },
+    });
+
+    if (!playList) {
+      return res.status(404).json({ error: "Playlist not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Playlist fetched successfully",
+      playList,
+    });
+  } catch (error) {
+    console.error("Error fetching playlist:", error);
+    res.status(500).json({ error: "Failed to fetch playlist" });
+  }
+};
 export const addProblemToPlaylist = async (req, res) => {};
 export const deletePlayList = async (req, res) => {};
 export const removeProblemFromPlaylist = async (req, res) => {};
