ce83a1ccbe34ab4a9acdb8280c05fd71e5696004
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-05-25 12:21:12 +0530

    Frontend - Created Homepage, loginPage and signupPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-  return <>App</>;
+  const [authUser, setAuthUser] = useState(null);
+  return (
+    <>
+      <div className="flex flex-col justify-start items-center">
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+          />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
